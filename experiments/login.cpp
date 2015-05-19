e2e8633d8fa7e5ce8a8a3f1810834602a35373b8
Matthew Sherborne <msherborne@gmail.com>
2015-05-19 20:41:58 +1000

    Can now get a token

--- experiments/login.cpp
+++ experiments/login.cpp
@@ -2,8 +2,12 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <iterator>
 
 #include <json_class.hpp>
+#include <parse_to_json_class.hpp>
+#include <curlpp11.hpp>
 
 int main(int argc, char** argv)
 {
@@ -16,9 +20,25 @@ int main(int argc, char** argv)
   std::string key = argv[2];
   // json should be { "auth":{ "RAX-KSKEY:apiKeyCredentials":{ "username":"${USER}", "apiKey":"${KEY}" } } }
   using namespace json;
-  json::JSON login(
+  JSON login(
       JMap{{"auth", JMap{{"RAX-KSKEY:apiKeyCredentials",
                           JMap{{"username", username}, {"apiKey", key}}}}}});
-  std::cout << login << std::endl;
+  /// Connect
+  std::stringstream stream;
+  std::string reply;
+  stream << login;
+  curl::Easy client;
+  client.url("https://auth.api.rackspacecloud.com/v2.0/tokens")
+      //.setOpt(CURLOPT_VERBOSE)
+      .header("Accept: application/json")
+      .header("Content-type: application/json")
+      .customBody(stream)
+      .POST()
+      .perform(reply);
+  // Parse the response
+  std::cout << "Resuponse Code: " << client.responseCode() << std::endl;
+  JSON data = json::readValue(reply.begin(), reply.end());
+  std::string token = data.at("access").at("token").at("id");
+  std::cout << "Token: " << token << std::endl;
   return 0;
 }
